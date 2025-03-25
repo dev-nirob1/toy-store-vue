7345ac6ce4277793544499dc1d4a8f7499640ecf
dev-nirob1 <dev.nirob1@gmail.com>
2025-03-25 19:47:40 +0600

    folder structure with routing

---
+++ src/components/element/BaseImage.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+  image: {
+    type: [String, Object],
+    required: true
+  }
+})
+</script>
+<template>
+    <img class="height-full width-full" :src="image" alt="banner image">
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
+import AboutPage from '@/zems/front/pages/AboutPage.vue'
+import ContactPage from '@/zems/front/pages/ContactPage.vue'
+import HomePage from '@/zems/front/pages/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +9,18 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: HomePage,
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: AboutPage
+      // component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactPage
     },
   ],
 })
