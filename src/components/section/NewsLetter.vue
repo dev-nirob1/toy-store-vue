fd1a0a3c0483d613de5b04ad3580e03935d08cb6
dev-nirob1 <dev.nirob1@gmail.com>
2025-04-05 15:05:06 +0600

    newsletter section done

--- src/components/section/NewsLetter.vue
+++ src/components/section/NewsLetter.vue
@@ -1,11 +1,37 @@
-<template>
- <section class="newsletter bg-secondary container">
+<script setup lang="ts">
 
- </section>
+</script>
+
+<template>
+  <section class="newsletter bg-primary container">
+   <div>
+    <BaseTitle>
+      Stay Updated
+    </BaseTitle>
+    <BaseParagraph>
+      Subscribe to our newsletter for the latest toys, exclusive offers, and more!
+    </BaseParagraph>
+    <div class="input-container flex">
+      <input type="email" placeholder="Please Enter Your Email">
+      <button class="btn bg-secondary">Subscribe</button>
+    </div>
+   </div>
+  </section>
 </template>
 
 <style scoped>
-.newletter {
-  height: 45vh;
+.newsletter {
+  height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center
+}
+.input-container{
+  max-width: 768px;
+  margin:0 auto;
+}
+.input-container input{
+  background-color: #fff;
 }
 </style>
